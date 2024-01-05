43c794abd1c18e2b0d5d4278360c3f5c282c9375
Ankush sonone <Ankush.sonone07@gmail.com>
2024-01-05 14:57:51 +0530

    my first commit

---
+++ force-app/main/default/lwc/serchResult/serchResult.js
@@ -0,0 +1,22 @@
+import { LightningElement ,track,wire } from 'lwc';
+import getaccount from '@salesforce/apex/AccountDetails.getDetails';
+
+export default class SerchResult extends LightningElement {
+    @track data;
+    @track error='';
+
+    @wire(getaccount) wireList({data,error})
+    {
+               if(data)
+                {
+                    this.data=data;
+                    window.alert(' this.data is --->'+ this.data);
+                }
+                else if(error)
+                {
+
+                    this.error=error;
+                }
+    }
+
+}
